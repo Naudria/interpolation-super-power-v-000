2f10436222f7286fae2243f14c214128d2d114e3
Rima Jean <rima.jean@gmail.com>
2018-01-18 22:34:49 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
-  colors = [ "red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  colors = [ "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
   puts "The colors of the rainbow are: #{colors}"
 end
